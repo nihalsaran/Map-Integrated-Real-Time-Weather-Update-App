ad4fc4e5801ed79e3a4a739258650fadfc7b4169
Nihal saran das duggirala <92871743+nihalsaran@users.noreply.github.com>
2024-04-05 23:25:15 +0530

    Added Dynamic Weather update based on user origin location

--- src/App.js
+++ src/App.js
@@ -29,51 +29,17 @@ function App() {
     libraries: ['places'],
   })
 
-  const [map, setMap] = useState(/** @type google.maps.Map */(null))
+  const [map, setMap] = useState(/** @type google.maps.Map */ (null))
   const [directionsResponse, setDirectionsResponse] = useState(null)
   const [distance, setDistance] = useState('')
   const [duration, setDuration] = useState('')
-  const [currentLocation, setCurrentLocation] = useState(null);
-  const [directions, setDirections] = useState([]); // New state for directions
-  const [currentWeather, setCurrentWeather] = useState(null);
+  const [directions, setDirections] = useState([])
+  const [currentWeather, setCurrentWeather] = useState(null)
 
   /** @type React.MutableRefObject<HTMLInputElement> */
   const originRef = useRef()
   /** @type React.MutableRefObject<HTMLInputElement> */
   const destinationRef = useRef()
-
-  useEffect(() => {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        const { latitude, longitude } = position.coords;
-        setCurrentLocation({ lat: latitude, lng: longitude });
-
-        // Fetch weather data using OpenWeatherMap API (replace with your API key)
-        const apiKey = 'c33f1e3c3e8e34f4848af33c1e58b4c0';
-        const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`; // Use 'metric' for Celsius, 'imperial' for Fahrenheit
-
-        axios.get(url)
-          .then(response => {
-            const weather = response.data.weather[0].main; // Assuming 'main' property holds weather description
-            const temp = Math.round(response.data.main.temp); // Assuming 'temp' property holds temperature in Kelvin, round to nearest integer
-
-            console.log(`Current weather at your location: ${weather}, ${temp}°C`);
-            setCurrentWeather(`${weather}, ${temp}°C`); // Update currentWeather with combined data
-          })
-          .catch(error => {
-            console.error('Error fetching weather data:', error);
-            // Handle error gracefully (e.g., display error message to user)
-          });
-      },
-      (error) => {
-        console.error('Error getting current location:', error);
-      }
-    );
-  } else {
-    console.error('Geolocation is not supported by this browser.');
-  }
-}, []);
 
   if (!isLoaded) {
     return <SkeletonText />
@@ -83,33 +49,70 @@ function App() {
     if (originRef.current.value === '' || destinationRef.current.value === '') {
       return
     }
-    // eslint-disable-next-line no-undef
-    const directionsService = new google.maps.DirectionsService()
-    const results = await directionsService.route({
-      origin: originRef.current.value,
-      destination: destinationRef.current.value,
-      provideRouteAlternatives: true,
-      // eslint-disable-next-line no-undef
-      travelMode: google.maps.TravelMode.DRIVING,
-    })
-    setDirectionsResponse(results)
-    setDistance(results.routes[0].legs[0].distance.text)
-    setDuration(results.routes[0].legs[0].duration.text)
 
-    // Parse directions for display
-    const turnByTurnInstructions = getTurnByTurnInstructions(results);
-    setDirections(turnByTurnInstructions);
+    if (window.google) {
+      const directionsService = new window.google.maps.DirectionsService()
+      const results = await directionsService.route({
+        origin: originRef.current.value,
+        destination: destinationRef.current.value,
+        provideRouteAlternatives: true,
+        travelMode: window.google.maps.TravelMode.DRIVING,
+      })
+
+      setDirectionsResponse(results)
+      setDistance(results.routes[0].legs[0].distance.text)
+      setDuration(results.routes[0].legs[0].duration.text)
+
+      const turnByTurnInstructions = getTurnByTurnInstructions(results)
+      setDirections(turnByTurnInstructions)
+
+      // Fetch weather data for the origin location
+      const originLocation = await getLatLngFromAddress(originRef.current.value)
+      fetchWeatherData(originLocation)
+    } else {
+      console.error('Google Maps API is not loaded.')
+    }
+  }
+
+  async function getLatLngFromAddress(address) {
+    if (window.google) {
+      const geocoder = new window.google.maps.Geocoder()
+      const response = await geocoder.geocode({ address })
+      const location = response.results[0].geometry.location
+      return location
+    } else {
+      console.error('Google Maps API is not loaded.')
+      return null
+    }
+  }
+
+  async function fetchWeatherData(location) {
+    if (location) {
+      const lat = location.lat()
+      const lng = location.lng()
+      const apiKey = 'c33f1e3c3e8e34f4848af33c1e58b4c0'
+      const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${apiKey}&units=metric`
+
+      try {
+        const response = await axios.get(url)
+        const weather = response.data.weather[0].main
+        const temp = Math.round(response.data.main.temp)
+        setCurrentWeather(`${weather}, ${temp}°C`)
+      } catch (error) {
+        console.error('Error fetching weather data:', error)
+      }
+    }
   }
 
   function getTurnByTurnInstructions(directionsResponse) {
-    const instructions = [];
-    const legs = directionsResponse.routes[0].legs;
+    const instructions = []
+    const legs = directionsResponse.routes[0].legs
     for (const leg of legs) {
       for (const step of leg.steps) {
-        instructions.push(step.instructions);
+        instructions.push(step.instructions)
       }
     }
-    return instructions;
+    return instructions
   }
 
   function clearRoute() {
@@ -118,10 +121,11 @@ function App() {
     setDuration('')
     originRef.current.value = ''
     destinationRef.current.value = ''
-    setDirections([]); // Clear directions state
+    setDirections([])
+    setCurrentWeather(null)
   }
 
-  const center = currentLocation || { lat: 48.8584, lng: 2.2945 };
+  const center = { lat: 48.8584, lng: 2.2945 }
 
   return (
     <Flex
@@ -161,7 +165,7 @@ function App() {
         zIndex='1'
       >
         <HStack spacing={2} justifyContent='space-between'>
-        <Box flexGrow={1}>
+          <Box flexGrow={1}>
             <Autocomplete>
               <Input type='text' placeholder='Origin' ref={originRef} />
             </Autocomplete>
@@ -203,41 +207,41 @@ function App() {
 
         {/* Display Turn-by-Turn Directions */}
         {directions.length > 0 && (
-         <Box mt={4} bgColor="gray.100" p={4} borderRadius="md"  maxHeight="300px" overflowY="auto">
-         <Text fontSize="lg" fontWeight="bold" mb={2}>Turn-by-Turn Directions:</Text>
-         <ol style={{ listStyleType: 'decimal', paddingLeft: '20px', margin: '0' }}>
-           {directions.map((instruction, index) => (
-             <li key={index} style={{ marginBottom: '8px' }}>{instruction.replace(/<[^>]*>?/gm, '')}</li>
-           ))}
-         </ol> 
-         
-       </Box>
+          <Box mt={4} bgColor="gray.100" p={4} borderRadius="md" maxHeight="300px" overflowY="auto">
+            <Text fontSize="lg" fontWeight="bold" mb={2}>Turn-by-Turn Directions:</Text>
+            <ol style={{ listStyleType: 'decimal', paddingLeft: '20px', margin: '0' }}>
+              {directions.map((instruction, index) => (
+                <li key={index} style={{ marginBottom: '8px' }}>{instruction.replace(/<[^>]*>?/gm, '')}</li>
+              ))}
+            </ol>
+
+          </Box>
         )}
         {/* Weather box outside the main box */}
         {currentWeather && (
-  <Box
-    position='absolute'
-    bottom='30px'
-    right='700px'
-    zIndex='2'
-    bgColor='white'
-    p={2}
-    borderRadius='lg'
-    shadow='base'
-  >
-    <Text fontWeight='bold'>Current Weather:</Text>
-    <Text>
-      Weather:
-      {currentWeather.split(',')[0]}  {/* Display weather description */}
-      <br />  {/* Add a line break */}
-      <Text fontSize='sm' color='gray.500'> Temperature: {/* Style temperature text smaller and gray */}
-        {currentWeather.split(',')[1]}  {/* Display temperature */}
-      </Text>
-    </Text>
-  </Box>
-)}
+          <Box
+            position='absolute'
+            bottom='30px'
+            right='700px'
+            zIndex='2'
+            bgColor='white'
+            p={2}
+            borderRadius='lg'
+            shadow='base'
+          >
+            <Text fontWeight='bold'>Current Weather:</Text>
+            <Text>
+              Weather:
+              {currentWeather.split(',')[0]}  {/* Display weather description */}
+              <br />  {/* Add a line break */}
+              <Text fontSize='sm' color='gray.500'> Temperature: {/* Style temperature text smaller and gray */}
+                {currentWeather.split(',')[1]}  {/* Display temperature */}
+              </Text>
+            </Text>
+          </Box>
+        )}
       </Box>
-      
+
     </Flex>
   )
 }
